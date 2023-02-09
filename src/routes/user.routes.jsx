4e6de07e802fe3e5afb8b795a3a3a1a43208828e
Maino Estivalet <nmainoi@hotmail.com>
2023-02-09 16:18:17 -0300

    create user routes

--- src/routes/user.routes.jsx
+++ src/routes/user.routes.jsx
@@ -1,13 +1,12 @@
 
 import { Routes, Route} from 'react-router-dom';
-
+import { Home } from '../pages/home';
 
 
 export function UserRoutes(){
     return(
         <Routes>
-           <Route path="/" element={<SignIn/>}/> 
-           <Route path="/register" element={<SignUp/>}/> 
+           <Route path="/" element={<Home/>}/> 
         </Routes>
     )
 }
