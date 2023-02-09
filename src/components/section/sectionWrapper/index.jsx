abd8b2ff6b6b039f8a550da489ae66208b46b50d
Maino Estivalet <nmainoi@hotmail.com>
2023-02-09 15:11:14 -0300

    section buttons styled and logic

--- src/components/section/sectionWrapper/index.jsx
+++ src/components/section/sectionWrapper/index.jsx
@@ -1,12 +1,17 @@
-import { Container, CardSection, SectiondWrapper } from "./style";
-import { ArrowRigth } from "../../../assets/Icons";
+import { Container, CardSection, SectiondWrapper, LeftButton, RigthButton, IconWrapper } from "./style";
+import { SimpleArrowRigthIcon } from "../../../assets/Icons";
 import { Card } from "../card";
 
 import img from '../../../assets/Mask-group-1.png'
+import { useMediaQuery } from 'react-responsive';
 import { useEffect, useRef, useState } from "react";
 export function FoodSection({ title }) {
     const [scrollLeft, setsCrollLeft] = useState(0);
     const cardRef = useRef(null);
+
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 800px)'
+    })
 
     const data = {
         id: 1,
@@ -16,26 +21,42 @@ export function FoodSection({ title }) {
         img: img
     }
     useEffect(() => {
-
+        // console.log("ref",cardRef.current.scrollLeft)
+        // console.log("ScrollLeft",scrollLeft)
         cardRef.current.scrollTo({
             left: scrollLeft,
             top: 0,
             behavior: 'smooth'
         })
 
+
     }, [scrollLeft])
 
     function HandleScroll(isToLeft) {
 
+
         const maxScrollLeft = cardRef.current.scrollWidth - cardRef.current.clientWidth;
         const valueToScroll = 300;
+        const scrollLeft = cardRef.current.scrollLeft;
 
-        if (isToLeft && maxScrollLeft > cardRef.current.scrollLeft + valueToScroll) {
-            setsCrollLeft((prev) => prev + valueToScroll)
+
+
+
+
+
+        if (isToLeft) {
+            if (maxScrollLeft > cardRef.current.scrollLeft + valueToScroll)
+                setsCrollLeft(cardRef.current.scrollLeft + valueToScroll)
+            else 
+                setsCrollLeft(maxScrollLeft)
+            
         }
 
-        if (!isToLeft && cardRef.current.scrollLeft >= valueToScroll) {
-            setsCrollLeft((prev) => prev - valueToScroll)
+        if (!isToLeft ) {
+            if( cardRef.current.scrollLeft >= valueToScroll)
+            setsCrollLeft(cardRef.current.scrollLeft - valueToScroll)
+            else 
+            setsCrollLeft(0)
         }
 
         setsCrollLeft((prev) => prev);
@@ -45,23 +66,38 @@ export function FoodSection({ title }) {
         <Container>
             <h1 >{title}</h1>
 
-            <SectiondWrapper>
-                <CardSection ref={cardRef} >
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
-                    <Card img={img} data={data} />
+            <SectiondWrapper ref={cardRef}>
 
-
+                <CardSection  >
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
+                    <Card img={img} data={data} />
                 </CardSection>
-            </SectiondWrapper>
 
+            </SectiondWrapper>
+            {isDesktop ?
+                <LeftButton onClick={() => HandleScroll(false)}>
+                    <IconWrapper>
+                        <SimpleArrowRigthIcon />
+                    </IconWrapper>
+                </LeftButton>
+                : <></>}
+
+            {isDesktop ?
+                <RigthButton onClick={() => HandleScroll(true)} >
+                    <IconWrapper>
+                        <SimpleArrowRigthIcon />
+                    </IconWrapper>
+
+                </RigthButton>
+                : <></>}
 
         </Container>
     );
--- src/components/section/sectionWrapper/style.js
+++ src/components/section/sectionWrapper/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin-left: 10%;
+  margin-left: 10px;
   max-width: 100vw;
 
   > h1 {
@@ -20,19 +20,23 @@ color: ${({ theme }) => theme.COLORS.LIGHT_300};
  
  @media (min-width: 800px) {
     margin-left: 0;
+    position: relative;
  }
 `;
 
 export const SectiondWrapper = styled.div`
+
 
 overflow-x: scroll;
 max-width: 1110px;
 @media (min-width: 800px) {
     overflow-x: scroll;
 
+
     &::-webkit-scrollbar {
         display: none;
     }
+
  }
 
 `;
@@ -44,5 +48,56 @@ export const CardSection = styled.div`
   width: max-content;
     overflow-x: hidden;
     gap: 2rem;
+
+ padding-right: 10px;
+
+  @media (min-width: 800px) {
+    padding-right: 300px;
+  }
 `;
 
+
+export const LeftButton = styled.div`
+left: -1px;
+position: absolute;
+width: 224px;
+height: 292px;
+top: 20px;
+
+/* Gradients/100 */
+background: ${({ theme }) => theme.COLORS.GRADIENT_100};
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+transition: 0.5s;
+&:hover {
+    cursor: pointer;
+    opacity: 0.8;
+}
+`;
+
+
+export const RigthButton = styled.div`
+
+position: absolute;
+width: 224px;
+height: 292px;
+right: -1px;
+top: 20px;
+
+/* Gradients/100 */
+
+background: ${({ theme }) => theme.COLORS.GRADIENT_100};
+transition: 0.5s;
+&:hover {
+    cursor: pointer;
+    opacity: 0.8;
+}
+`;
+
+export const IconWrapper = styled.div`
+width: 24px;
+height: 36px;
+position: absolute;
+right: 0;
+top: 50%;
+`;
